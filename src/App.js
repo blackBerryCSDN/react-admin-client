b073e859799ce38a77b5c918fcb579ad79036f9f
unknown <1628715865@qq.com>
2020-12-15 11:41:48 +0800

    init app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, message } from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    handleClick = ()=> {
+        message.success('成功啦...');
+    }
+    render() {
+        return (
+            <div>
+                <Button type='primary' onClick={this.handleClick}>学习</Button>
+            </div>
+        )
+    }
 }
-
-export default App;
+export default App
